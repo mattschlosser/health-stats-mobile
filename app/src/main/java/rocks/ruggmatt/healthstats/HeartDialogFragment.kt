1b8945e5ba94d4992c54821ebc44b0eb6ea9fdbf
Matt Schlosser <mschlosser@albertabcsafety.com>
2022-06-22 22:01:37 -0600

    automatically show keyboard when entering heart reate

--- app/src/main/java/rocks/ruggmatt/healthstats/HeartDialogFragment.kt
+++ app/src/main/java/rocks/ruggmatt/healthstats/HeartDialogFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
@@ -50,6 +51,7 @@ class HeartDialogFragment : BottomSheetDialogFragment() {
     ): View? {
 
         _binding = FragmentHeartDialogBinding.inflate(inflater, container, false)
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return binding.root
 
     }
